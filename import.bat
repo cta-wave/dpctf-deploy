d52f61288da74ce220442eb60a71c042b1ad4ac0
Louay Bassbouss <louaybassbouss@users.noreply.github.com>
2021-09-30 09:50:51 +0200

    Update import script
    Use a version of database.json compatible with DPCTF tests

--- import.bat
+++ import.bat
@@ -3,4 +3,4 @@ setlocal enabledelayedexpansion
 
 
 echo Downloading content ...
-python download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/master/database.json content
+python download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/1197c7d793fefeeb2d3ecff2815c36f7e9e97581/database.json content
