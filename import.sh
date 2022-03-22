bc42366944d3e2d827c5749015ce982a5742ec51
Fritz Heiden <fritz.heiden@mailbox.org>
2022-03-22 18:04:24 +0100

    Explicitly call download script with python3

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading content ..."
-./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/587cef1f7d531bb9326c21ca11cad95454d62621/database.json content
+python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/587cef1f7d531bb9326c21ca11cad95454d62621/database.json content
 
 #echo ""
 #echo "Importing DPCTF tests ..."
