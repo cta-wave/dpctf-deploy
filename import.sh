5c1ee1cf2386ffc01ac61d05efa863d286e29cb9
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2023-04-14 17:43:01 +0200

    Updated import script to download new 2023-04-06 content

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading content ..."
-python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/587cef1f7d531bb9326c21ca11cad95454d62621/database.json content
+python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/cb0e50de1c21b1432263ffd295b88099a1d2a6e3/database.json content
 
 #echo ""
 #echo "Importing DPCTF tests ..."
