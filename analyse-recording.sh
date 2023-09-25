d45a6ca0d73b866955955c810d5b501dc3222728
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2023-09-25 14:37:50 +0200

    Applied changes to analyse script

--- analyse-recording.sh
+++ analyse-recording.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 if ! $(docker info > /dev/null 2>&1); then
-    echo "Docker is not running.";
+    echo "Unable to access docker. (Not running or no permissions?)";
     exit 1;
 fi
 
-if [[ "$(docker images -q dpctf-dof:latest 2> /dev/null)" == "" ]]; then
+if [ "$(docker images -q dpctf-dof:latest 2> /dev/null)" == "" ]; then
     echo "DOF image not found! Please build it using:";
     echo "docker build --file Dockerfile.dof -t dpctf-dof:latest .";
     exit 1;
@@ -21,8 +21,10 @@ absolute_path=$(realpath "$1");
 dirname=$(dirname "$absolute_path");
 filename=$(basename "$absolute_path");
 
+observation_ini=$(realpath "./observation-config.ini");
+
 docker run --rm --name device-observation-framework \
 -v "$dirname":/usr/app/recordings \
 -e RECORDING_FILENAME="$filename" \
--v ./observation-config.ini:/usr/app/device-observation-framework/config.ini \
+-v "$observation_ini":/usr/app/device-observation-framework/config.ini \
 dpctf-dof:latest
