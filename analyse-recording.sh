5ae813d34f6d09a3902c2ceb5d6d874e94d3b092
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2023-11-01 13:38:34 +0100

    Removed name from OF containers to enable multiple instances

--- analyse-recording.sh
+++ analyse-recording.sh
@@ -27,5 +27,4 @@ docker run -it --rm \
 -v "$dirname":/usr/app/recordings \
 -e RECORDING_FILENAME="$filename" \
 -v "$observation_ini":/usr/app/device-observation-framework/config.ini \
--v "$(pwd)/logs":/usr/app/device-observation-framework/logs \
 dpctf-dof:latest
