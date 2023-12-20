cd3095e9c8f3485b811fbe36e2c93c95297b4881
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2023-12-20 14:57:55 +0100

    Make import.bat use import.sh in python docker container

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading content ..."
-python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/3fa45e776c2bd46d44fe092ec5e35d3948f22ead/database.json content
+python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/3fa45e776c2bd46d44fe092ec5e35d3948f22ead/database.json content 
 
 #echo ""
 #echo "Importing DPCTF tests ..."
