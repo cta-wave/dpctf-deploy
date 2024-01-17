181c3aa46499c3df84364b51924bf404583216b6
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2024-01-17 16:04:57 +0100

    dockerize import.sh

---
+++ download-content.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Downloading content ..."
+python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/3fa45e776c2bd46d44fe092ec5e35d3948f22ead/database.json content 
--- import.sh
+++ import.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-echo "Downloading content ..."
-python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/3fa45e776c2bd46d44fe092ec5e35d3948f22ead/database.json content 
-
-#echo ""
-#echo "Importing DPCTF tests ..."
-#git clone https://github.com/cta-wave/dpctf-tests dpctf
-#mv dpctf/generated/* tests
-#if [ ! -f tests/test-config.json ]; then
-#mv dpctf/test-config.json tests
-#fi
-#rm -rf dpctf
+docker run -it --rm --name import-content -v $(pwd):/usr/src/import-content -w /usr/src/import-content python:3.8 sh ./download-content.sh
