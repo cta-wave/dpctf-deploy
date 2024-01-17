bd2512cdb5fe53031c0a4e70b79b72e6855dbff6
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2024-01-17 16:07:06 +0100

    feat: dockerize import.sh (#60)

--- import.bat
+++ import.bat
@@ -5,4 +5,4 @@ setlocal enabledelayedexpansion
 ::echo Downloading content ...
 ::python download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/75eecb6a5a558fddf8bbdd68c6a5b3675be55f0c/database.json content
 
-docker run -it --rm --name import-content -v %cd%:/usr/src/import-content -w /usr/src/import-content python:3.10 sh ./import.sh
+docker run -it --rm --name import-content -v %cd%:/usr/src/import-content -w /usr/src/import-content python:3.8 sh ./download-content.sh
