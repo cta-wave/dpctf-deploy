f06b582e7c010c059b0ce8847d1bb2e0d38cfe26
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2024-06-07 12:53:04 +0200

    feat: set tag v2.0.0 in build.bat

--- build.bat
+++ build.bat
@@ -5,12 +5,28 @@ set reload_runner=0
 set reload_tests=0
 set tests_branch="master"
 set has_tests_branch=0
+set test_runner_commit="v2.1.0"
+set image_tag="v2.0.0"
+set argument_count=0
 
 for %%x in (%*) do (
-   if %has_tests_branch%==1 set tests_branch="%%~x"
-   if "%%~x"=="--reload-runner" set reload_runner=1
-   if "%%~x"=="--reload-tests" set reload_tests=1
-   if "%%~x"=="--tests-branch" set has_tests_branch=1
+    if %has_tests_branch%==1 (
+        set tests_branch="%%~x"
+        set has_tests_branch=0
+    ) else (
+        if not "%%~x:~0,2%%"=="--" (
+            if %argument_count%==0 (
+                set test_runner_commit="%%~x"
+                set argument_count=1
+            ) else if %argument_count%==1 (
+                set image_tag="%%~x"
+                set argument_count=2
+            )
+        )
+    )
+    if "%%~x"=="--reload-runner" set reload_runner=1
+    if "%%~x"=="--reload-tests" set reload_tests=1
+    if "%%~x"=="--tests-branch" set has_tests_branch=1
 )
 
 set args=
@@ -18,7 +34,7 @@ set args=
 if %reload_runner%==1 set args=%args% --build-arg runner-rev=%DATE%
 if %reload_tests%==1 set args=%args% --build-arg runner-rev=%DATE%
 
-set command=docker build --network="host" --build-arg commit=%1%args% --build-arg testsbranch=%tests_branch% -t dpctf:%2 .
+set command=docker build --network="host" --build-arg commit=%test_runner_commit%%args% --build-arg testsbranch=%tests_branch% -t dpctf:%image_tag% .
 
 echo %command%
 
