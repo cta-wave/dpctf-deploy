60d7d662104dcc03e4db139730e289262cd430cc
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2024-09-18 13:36:43 +0200

    feat: bump version of dof from v2.0.0 to 2.0.1 (#113)

--- build-dof.sh
+++ build-dof.sh
@@ -20,4 +20,4 @@ if [ $reload_dof = true ]; then
   args="$args --build-arg dof-rev=\"$(date | sed "s/ //g")\""
 fi
 
-docker build $args --file Dockerfile.dof -t dpctf-dof:v2.0.0 .
+docker build $args --file Dockerfile.dof -t dpctf-dof:v2.0.1 .
