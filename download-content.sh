03a0cb6c6e649c5ce6bf3242226c2906b1ff016c
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2025-09-03 13:40:32 +0200

    feat: use latest database.json for downloading content

--- download-content.sh
+++ download-content.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Downloading content ..."
-python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/v2.0.0/database.json content 
+python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/master/database.json content
