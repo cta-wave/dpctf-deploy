fd3e2a6f31b0c11d8c02a620ddcb33828c7b88ee
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2026-02-18 10:52:27 +0100

    feat: update referenced database.json

--- download-content.sh
+++ download-content.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Downloading content ..."
-    python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/bb7022d8a815c8d98259f2e019c1b88db7d6d8a2/database.json content
+    python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/997150418f6e4db09a2a391884959d282b377470/database.json content
