ba6e1d03aec546b69712de222160ff651f260050
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2026-03-30 16:34:50 +0200

    feat: bump referenced versions to v4.0.0 (#139)

--- download-content.sh
+++ download-content.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Downloading content ..."
-    python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/997150418f6e4db09a2a391884959d282b377470/database.json content
+    python3 ./download-content.py https://raw.githubusercontent.com/cta-wave/Test-Content/v4.0.0/database.json content
